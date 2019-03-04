0a9cdd37dc40d1df7cc419f66fb837d3157fd85b
El Dario Howard <eldario@cisadmins-macbook-air.local>
2019-03-04 10:30:20 -0600

    Initial Commit

---
+++ randomrestaurantTests/randomrestaurantTests.swift
@@ -0,0 +1,34 @@
+//
+//  randomrestaurantTests.swift
+//  randomrestaurantTests
+//
+//  Created by El Dario on 3/4/19.
+//  Copyright © 2019 El Dario. All rights reserved.
+//
+
+import XCTest
+@testable import randomrestaurant
+
+class randomrestaurantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
